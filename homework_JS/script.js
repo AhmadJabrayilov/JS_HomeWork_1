c6a4e17389b075437e2db9a11c2378a058b5566e
AhmadJabrayilov <99828519+AhmadJabrayilov@users.noreply.github.com>
2022-05-05 21:47:00 -0800

    Add files via upload

---
+++ homework_JS/script.js
@@ -0,0 +1,9 @@
+// let list1 = document.getElementById("list-3").style.listStyle="none"
+let list1 = document.getElementById("list1-item3").innerText="C#"
+let list2 = document.getElementById("list2-item3").innerText="Js"
+let list3 = document.getElementById("list3-item3").innerText="C++"
+let list4 = document.getElementById("list4-item3").innerText="Ruby"
+let list5 = document.getElementById("list5-item3").innerText="Go"
+
+
+
